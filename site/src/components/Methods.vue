d1fb83fe3955200cc64ca62e416bbb66098b889f
Jeff MacInnes <jeff.macinnes@gmail.com>
2020-10-22 11:53:54 -0700

    added census link:

--- site/src/components/Methods.vue
+++ site/src/components/Methods.vue
@@ -3,14 +3,14 @@
       <div class='col-container'>
         <div class='col'>
           <p>
-            <sup>*</sup>Voter turnout within a region was defined as the total votes divided by the voting-age population (VAP). Using VAP as the denominator is less accurate than using voting-eligible population (VEP), however VEP data was not available at a county level. For more information on this distinction, please see <a href="http://www.electproject.org/2016g">US Elections Project</a>
+            <sup>*</sup>Voter turnout within a region was defined as the total votes divided by the voting-age population (VAP). VAP is less accurate than using the voting-eligible population (VEP), however VEP data was not available at a county level. For more information on this distinction, please see <a href="http://www.electproject.org/2016g">US Elections Project</a>
           </p>
 
         </div>
 
         <div class='col'>
           <p>
-            Designed and developed by <a href="https://www.jeffmacinnes.com">Jeff MacInnes</a> and <a href="https://www.sergeilarionov.com">Sergei Larionov</a>. County-level vote totals from <a href="http://www.townhall.com/election/2016/president">Townhall</a> (web-scraping approach developed by <a href="https://github.com/tonmcg/US_County_Level_Election_Results_08-16">Tony McGovern</a>). Visualization created and embedded via <a href="https://observablehq.com/d/2c3d3b4b0ec5f2a3">ObservableHQ</a>.
+            Designed and developed by <a href="https://www.jeffmacinnes.com">Jeff MacInnes</a> and <a href="https://www.sergeilarionov.com">Sergei Larionov</a>. County-level vote totals from <a href="http://www.townhall.com/election/2016/president">Townhall</a> (web-scraping approach developed by <a href="https://github.com/tonmcg/US_County_Level_Election_Results_08-16">Tony McGovern</a>). Voting-age population statistics from <a href="https://www.census.gov/programs-surveys/decennial-census/about/voting-rights/cvap.2016.html.">US Census</a>. Visualization created and embedded via <a href="https://observablehq.com/d/2c3d3b4b0ec5f2a3">ObservableHQ</a>.
           </p>
         </div>
 
